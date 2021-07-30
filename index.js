885e55e2ab57bc5b19f3c43dccc9deffb6a33393
Hemant Panwar <45938556+hemant2132@users.noreply.github.com>
2021-07-30 12:18:26 +0530

    Create web server

--- index.js
+++ index.js
@@ -40,3 +40,11 @@ const updateRows = require("./src/spreadsheet/updateRows");
 
   routine();
 })();
+
+// web server
+const http = require("http");
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end("ok");
+});
+server.listen(3000);
